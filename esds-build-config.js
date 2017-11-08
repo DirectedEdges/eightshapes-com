64c09188d5f7d3b63cdae3a0b2a558c4fac06d0a
Kevin Powell <kevin@eightshapes.com>
2017-11-08 07:42:36 -0600

    Update responsive grid demo and case studies pages

--- esds-build-config.js
+++ esds-build-config.js
@@ -11,5 +11,22 @@ module.exports = {
             obj[key] = value;
             return obj;
         });
-    }
+    },
+    copyTasks: [
+        {
+            name: 'responsive-grid-demo-styles',
+            sources: ['styles/responsive_grid_demo/*.css'],
+            destination: 'docs/styles/responsive_grid_demo'
+        },
+        {
+            name: 'responsive-grid-demo-scripts',
+            sources: ['scripts/responsive_grid_demo/*.js'],
+            destination: 'docs/scripts/responsive_grid_demo'
+        },
+        {
+            name: 'legacy-scripts',
+            sources: ['scripts/legacy/*.js'],
+            destination: 'docs/scripts/legacy'
+        }
+    ]
 }
