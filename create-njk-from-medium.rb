1861b3c309dbb3ec00ab92d880b5177ae702f22f
Kevin Powell <kevin@eightshapes.com>
2018-11-01 08:28:37 -0500

    Extract title, subtitle, publish date and read duration

--- create-njk-from-medium.rb
+++ create-njk-from-medium.rb
@@ -14,7 +14,21 @@ puts "Retrieving markup from : #{medium_url}"
 doc = Nokogiri::HTML(open(medium_url),&:noblanks)
 
 # Start formatting medium HTML
+published_date = doc.css('.postMetaInline time')
+published_date = published_date.empty? ? '' : published_date.text
+
+read_duration = doc.css('.postMetaInline .readingTime')
+read_duration = read_duration.empty? ? '' : read_duration.attr('title').text.gsub('min read', '')
+
 article_html = doc.css('.postArticle-content')
+title = article_html.css('.graf--title')
+title = title.empty? ? '' : title.text
+
+deck = article_html.css('.graf--subtitle')
+deck = deck.empty? ? '' : deck.text
+
+article_html.search('.graf--title').remove
+article_html.search('.graf--subtitle').remove
 
 medium_attributes_to_remove = ['data-post-id', 'data-source', 'data-collection-id', 'data-tracking-context', 'name', 'data-height' 'data-image-id', 'data-height', 'data-width', 'data-action', 'data-action-value', 'class', 'id', 'data-href', 'style']
 
@@ -28,21 +42,22 @@ filename = medium_url.gsub(/https:\/\/medium.com\/eightshapes-llc\//, '')
 last_hypen_index = filename.rindex(/-/)
 filename.slice!(last_hypen_index, filename.length)
 
-article_template_pre = "{% set title = '' %}
+article_template_pre = "{% set title = '#{title}' %}
 {% set title_image_path = title | lower | replace(' ','-') %}
-{% set deck = '' %}
+{% set deck = '#{deck}' %}
 {% set author = 'Nathan Curtis' %}
 {% set author_role = 'Founder of UX firm @eightshapes. Speaker. Writer. Fan of Arsenal, Hokies. Cyclist & runner. Father & husband. VT & @uchicago grad.' %}
-{% set published_date = '' %}
-{% set read_duration = '' %}
+{% set published_date = '#{published_date}' %}
+{% set read_duration = '#{read_duration}' %}
 {% set masthead = false %}
 
 {% set title_tag = 'EightShapes > Articles >'  + title %}
 {% extends 'templates/article.njk' %}
 
-{% block main_article %}\n"
+{% block main_article %}
+  {% filter markdown %}\n"
 
-article_template_post = "\n\n{% endblock %}"
+article_template_post = "\n\n  {% endfilter %}\n{% endblock %}"
 
 article_content = "#{article_template_pre}#{article_html.to_xhtml( indent:2, indent_text:" " )}#{article_template_post}"
 # article_content = "#{article_template_pre}#{article_html}#{article_template_post}"
@@ -58,20 +73,23 @@ puts "Downloading images:"
 featured_image = article_html.css("img[data-is-featured='true']")
 images = article_html.css("img:not([data-is-featured='true'])")
 
-if featured_image
+puts featured_image
+
+unless featured_image.empty?
   puts "Downloading Masthead image: #{featured_image.attr('src')}"
   File.open("#{article_image_directory}/Masthead.png", 'wb') do |fo|
     fo.write open(featured_image.attr('src')).read 
   end
 end
 
-images.each_with_index do |i, index|
-  puts "Downloading Article image: #{i.attr('src')}"
-  File.open("#{article_image_directory}/#{index + 1}.png", 'wb') do |fo|
-    fo.write open(i.attr('src')).read 
+unless images.empty?
+  images.each_with_index do |i, index|
+    puts "Downloading Article image: #{i.attr('src')}"
+    File.open("#{article_image_directory}/#{index + 1}.png", 'wb') do |fo|
+      fo.write open(i.attr('src')).read 
+    end
   end
 end
-
 
 
 # File.open('pie.png', 'wb') do |fo|
