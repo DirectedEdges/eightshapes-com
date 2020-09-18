f6c8b5c179fa25168516e48e1faeb61f2bd2efd0
Kevin Powell <kevin@eightshapes.com>
2020-09-18 12:18:27 -0500

    Add trailing slashes to redirects

--- build-tasks/create-redirects.js
+++ build-tasks/create-redirects.js
@@ -2,15 +2,15 @@ const fs = require('fs');
 const path = require('path');
 
 const redirects = {
-  '/nathan-curtis.html': '/nathan-curtis',
-  '/dan-brown.html': '/dan-brown',
-  '/kevin-powell.html': '/kevin-powell',
-  '/james-melzer.html': '/james-melzer',
-  '/systems/assessment.html': '/systems/assessment',
-  '/systems/workshops.html': '/systems/workshops',
-  '/discovery/crafting-discovery.html': '/discovery/crafting-discovery',
-  '/design-discovery.html': '/design-discovery',
-  '/design-systems.html': '/design-systems'
+  '/nathan-curtis.html': '/nathan-curtis/',
+  '/dan-brown.html': '/dan-brown/',
+  '/kevin-powell.html': '/kevin-powell/',
+  '/james-melzer.html': '/james-melzer/',
+  '/systems/assessment.html': '/systems/assessment/',
+  '/systems/workshops.html': '/systems/workshops/',
+  '/discovery/crafting-discovery.html': '/discovery/crafting-discovery/',
+  '/design-discovery.html': '/design-discovery/',
+  '/design-systems.html': '/design-systems/'
 }
 
 for (oldPage in redirects) {
