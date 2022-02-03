129fbe478b0e54bdacf2c23299526ca6a81f6e11
Kevin Powell <kevin@eightshapes.com>
2022-02-03 14:27:30 -0600

    Add article stylesheet

--- .eleventy.js
+++ .eleventy.js
@@ -5,66 +5,14 @@ const marked = require("marked"),
   pluginDate = require("eleventy-plugin-date");
 
 module.exports = function (eleventyConfig) {
-  // Nunjucks Filter
-  eleventyConfig.addNunjucksFilter("updateobj", function (obj, key, value) {
-    if (obj === undefined) {
-      obj = {};
-    }
-    obj[key] = value;
-    return obj;
-  });
-
-  eleventyConfig.addNunjucksFilter("markdown", function (
-    // This filter is used by nested esds and escom components. Without it, the build fails
-    string,
-    includeWrapper,
-    wrapperClass
-  ) {
-    var renderedMarkup = marked(stripIndent(string)),
-      configWrapperClass = "esds-markdown-wrap",
-      configIncludeMarkdownWrapper = false;
-
-    // If includeWrapper is set when the filter is called, use that value, otherwise use config defaults
-    includeWrapper =
-      typeof includeWrapper === "undefined"
-        ? configIncludeMarkdownWrapper
-        : includeWrapper;
-
-    // If a wrapperClass is set when the filter is called, use that value, otherwise use config defaults
-    wrapperClass =
-      typeof wrapperClass === "undefined" ? configWrapperClass : wrapperClass;
-
-    if (includeWrapper) {
-      renderedMarkup =
-        '<div class="' + wrapperClass + '">' + renderedMarkup + "</div>";
-    }
-
-    const env = new Nunjucks.Environment();
-
-    return env.filters.safe(renderedMarkup);
-    //   return renderedMarkup;
-  });
-
   eleventyConfig.setTemplateFormats(["html", "md", "njk"]);
 
   eleventyConfig.addPassthroughCopy("dist");
   eleventyConfig.addPassthroughCopy({ "src/CNAME": "CNAME" });
   eleventyConfig.addPassthroughCopy({ "src/_scripts": "scripts" });
   eleventyConfig.addPassthroughCopy({ "src/_images": "images" });
-  eleventyConfig.addPassthroughCopy({ "src/_icons": "icons" });
-  eleventyConfig.addPassthroughCopy({
-    "node_modules/esds-library/dist": "dependencies",
-  });
-  eleventyConfig.addPassthroughCopy({
-    "node_modules/esds-library/dist/esds.svg": "icons/esds.svg",
-  });
-  eleventyConfig.addPassthroughCopy({
-    "node_modules/@eightshapes/esds-code-snippet/dist": "dependencies",
-  });
-  eleventyConfig.addPassthroughCopy({
-    "node_modules/@webcomponents/custom-elements/custom-elements.min.js":
-      "dependencies/custom-elements.min.js",
-  });
+  eleventyConfig.addPassthroughCopy({ "src/_styles/*.css": "styles" });
+
   eleventyConfig.addWatchTarget("./templates");
   eleventyConfig.addWatchTarget("src/_styles");
   eleventyConfig.setWatchThrottleWaitTime(2000);
